01e15a48b736581b3ad73100fa34bcb6fa72a934
Roman <roman.shinyavski@gmail.com>
2022-07-06 12:21:57 +0200

    Initial Commit

---
+++ AH-Assignment-MVVMUITests/AH_Assignment_MVVMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AH_Assignment_MVVMUITestsLaunchTests.swift
+//  AH-Assignment-MVVMUITests
+//
+//  Created by Roman on 06/07/2022.
+//
+
+import XCTest
+
+class AH_Assignment_MVVMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
