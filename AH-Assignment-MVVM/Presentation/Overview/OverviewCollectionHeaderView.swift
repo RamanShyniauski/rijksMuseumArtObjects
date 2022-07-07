5e8f7e6a4df7cf8db189a8659c0c6f7f7532ddcf
Roman <roman.shinyavski@gmail.com>
2022-07-07 16:52:02 +0200

    - add OverviewCollectionHeaderView

---
+++ AH-Assignment-MVVM/Presentation/Overview/OverviewCollectionHeaderView.swift
@@ -0,0 +1,43 @@
+//
+//  OverviewCollectionHeaderView.swift
+//  AH-Assignment-MVVM
+//
+//  Created by Roman on 07/07/2022.
+//
+
+import SnapKit
+import UIKit
+
+class OverviewCollectionHeaderView: UICollectionReusableView {
+        
+    private lazy var labelView: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24)
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.8
+        label.textAlignment = .left
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .lightGray.withAlphaComponent(0.5)
+        addSubview(labelView)
+        labelView.snp.makeConstraints { make in
+            make.verticalEdges.equalTo(0)
+            make.leading.equalTo(10)
+            make.trailing.equalTo(-10)
+        }
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(with title: String) {
+        labelView.text = title
+    }
+}
