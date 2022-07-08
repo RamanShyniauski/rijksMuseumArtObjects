440a979c2f87c34922a4eb8c969369d6a7617361
Roman <roman.shinyavski@gmail.com>
2022-07-08 11:56:14 +0200

    - add OverviewSection model

---
+++ AH-Assignment-MVVM/Model/Overview/OverviewSection.swift
@@ -0,0 +1,13 @@
+//
+//  OverviewSection.swift
+//  AH-Assignment-MVVM
+//
+//  Created by Roman on 07/07/2022.
+//
+
+import Foundation
+
+struct OverviewSection {
+    let name: String
+    let cells: [OverviewCollectionViewCellModel]
+}
